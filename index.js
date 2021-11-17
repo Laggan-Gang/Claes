8efbe5afcec07ee046c9fcb7153570664ad29642
snygghugo <91157834+snygghugo@users.noreply.github.com>
2021-11-17 03:51:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const applyText = (canvas, text) => {
 		// Assign the font to the context and decrement it so it can be measured again
 		context.font = `${fontSize -= 10}px sans-serif`;
 		// Compare pixel width of the text to the canvas minus the approximate avatar size
-	} while (context.measureText(text).width > canvas.width - 300);
+	} while (context.measureText(text).width > canvas.width);
 
 	// Return the result to use in the actual canvas
 	return context.font;
