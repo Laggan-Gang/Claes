b00d4fe2e21a568ba4bb331b32ce05984fcfa650
Maakep <schon.fredrik@gmail.com>
2022-01-22 00:21:42 +0100

    Fix not writing message on fix

--- index.js
+++ index.js
@@ -443,6 +443,7 @@ client.on("messageCreate", async (meddelande) => {
 
     if (res.success) {
       meddelande.react("✅");
+      meddelande.reply(res.message);
     } else {
       meddelande.react("❌");
       meddelande.reply(svampbob(res.message));
