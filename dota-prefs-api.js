a347b3043880e8127d88efc5cd331de65c4738ce
Maakep <schon.fredrik@gmail.com>
2022-01-22 22:36:48 +0100

    Format another one

--- dota-prefs-api.js
+++ dota-prefs-api.js
@@ -1,5 +1,5 @@
-const { dotaPrefsBaseUrl } = require("./config.json");
-const fetch = require("node-fetch");
+const { dotaPrefsBaseUrl } = require('./config.json');
+const fetch = require('node-fetch');
 
 const UPDATE = `${dotaPrefsBaseUrl}/role`;
 const GENERATE = `${dotaPrefsBaseUrl}/roles`;
@@ -7,31 +7,31 @@ const DELETE = `${dotaPrefsBaseUrl}/:user`;
 
 module.exports = {
   parseMessage: async (message) => {
-    const arr = message.split(" ");
+    const arr = message.split(' ');
     const command = arr[1];
     const parameters = arr.slice(2);
 
     let res = {
       success: false,
       message:
-        "Available commands: set | roll | delete \r\n Example: `!dota set EternaLEnVy 1 5 2 fill 4 3`",
+        'Available commands: set | roll | delete \r\n Example: `!dota set EternaLEnVy 1 5 2 fill 4 3`',
     };
 
     switch (command) {
-      case "set":
+      case 'set':
         const [user, ...roles] = parameters;
         res = await update(user, roles);
         break;
-      case "roll":
+      case 'roll':
         const users = parameters;
         res = await generate(users);
         break;
-      case "delete":
+      case 'delete':
         const brukare = parameters[0];
         res = await del(brukare);
         break;
-      case "link":
-        res.success = false;
+      case 'link':
+        res.success = true;
         res.message = dotaPrefsBaseUrl;
         break;
     }
@@ -42,13 +42,13 @@ module.exports = {
 
 async function update(user, roles) {
   const res = await fetch(UPDATE, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       user: user,
       roles: roles,
     }),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
@@ -62,13 +62,13 @@ async function update(user, roles) {
 
 async function generate(users) {
   const res = await fetch(GENERATE, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       users: users,
       json: false,
     }),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
@@ -81,13 +81,13 @@ async function generate(users) {
 }
 
 async function del(user) {
-  const res = await fetch(DELETE.replace(":user", user), {
-    method: "DELETE",
+  const res = await fetch(DELETE.replace(':user', user), {
+    method: 'DELETE',
   });
 
   const success = res.status == 200;
   return {
     success: success,
-    message: success ? "Deleted" : "Something went wrong",
+    message: success ? 'Deleted' : 'Something went wrong',
   };
 }
