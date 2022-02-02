cb3fecaed93aba062c42a8c934328153869b281f
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-02-03 00:09:54 +0100

    Update claes.js

--- claes.js
+++ claes.js
@@ -600,7 +600,8 @@ client.on('messageCreate', async (meddelande) => {
     }
   }
   if (meddelande.author.id == '199914493570973697' && aleaIactaEst < 5) {
-    meddelande.reply('This you?', { files: ['august.png'] });
+    meddelande.reply('This you?');
+    meddelande.reply({ files: ['august.png'] });
   }
   if (/^(prevedi|преведи)/.test(meddelande.content)) {
     //h e l p
