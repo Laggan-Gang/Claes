7cfbfa12e89b03bd84b05b5d21d7ae0e8f7ec242
August Wiklund <augwik@gmail.com>
2022-02-08 21:27:32 +0100

    Added tic tac toe to claes

--- gamer.js
+++ gamer.js
@@ -17,7 +17,8 @@ module.exports = {
         'Du är inte ens ett något',
       matchid: 0,
       vinnare: undefined,
-    };  
+    }; 
+    // {}.toString.call(något) 
     if(typeof(något) == "object"){
       if(typeof(något.content) == "string"){
         // Hit kommer alla meddelanden!
@@ -32,6 +33,13 @@ module.exports = {
         console.log(spel)
   
         switch (spel) {
+          case 'treirad':
+          case 'tictactoe':
+          case 'ttt':
+          case 'tir':
+            hurGårDet = await treIRad(hurGårDet,något.author,något.mentions.users.at(0));
+            console.log("Nu är det färdigtreiradat.")
+            break;
           case 'stensaxpåse':
           case 'rockpaperscissors':
           case 'ssp':
@@ -77,96 +85,180 @@ async function förklaraVadChallengeGörIEttFintMeddelande(hurGårDet,något){
  
 // TRE I RAD!!1
     
-//async function treIRad(hurGårDet,utmanare,utmanad,igen = false) {
-//  plan = [ [0,0,0],[0,0,0],[0,0,0] ]
-//  rundaDelatPåTvå = 0 // Det blir ju max 9 rundor = 8 rundor + en som är tvingad = 4*2 rundor
-//  över = false
-//  console.log("Börjar ny tre-i-rad-match.")
-//  while(!över){ //Loopa här tills någon vinner
-//    plan = await skickaTreIRadUtmaning(plan,KRYSS,utmanare,igen)
-//    över = harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)
-//    hurGårDet.vinnare = utmanare
-//    if(!över){
-//      plan = await skickaTreIRadUtmaning(plan,CIRKEL,utmanad,igen)  
-//      över = harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)
-//      hurGårDet.vinnare = utmanad
-//    }
-//    rundaDelatPåTvå += 1
-//    if(!över %% rundaDelatPåTvå > 3){
-//      // Gör det sista draget automatiskt och kolla ifall någon vann
-//      for (let i = 0; i < 3; i++){
-//        if(plan[i].includes[0]){
-//          plan[i][plan[i].indexOf[0]] = 1 //1 börjar alltid så hen har sista draget
-//        }
-//      }
-//      if(harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)){
-//        hurGårDet.vinnare = utmanare
-//      } else {
-//        hurGårDet.vinnare = undefined
-//        över = true
-//      }
-//    }
-//  }
-//  hurGårDet.jättebra = true
-//  hurGårDet.meddelande = "Rätt bra faktiskt!"
-//  if(hurGårDet.vinnare ~undefined){
-//    hurGårDet.vinnare.send("You won! Congratulations :)")
-//  }
-//  return hurGårDet 
-//}
-//
-//async function skickaTreIRadUtmaning( plan, EMOJI, kämpe, igen = false){
-//  const jagLovarPåHederOchSamvete = new Promise(async (jagKirrar, jagBajsar) => {
-//    let pick = undefined
-//    
-//    if(igen) {
-//      sträng = KVADRAT+KVADRAT+KVADRAT
-//    } else {
-//      sträng = KVADRAT+KVADRAT+KVADRAT
-//    }
-//    
-//    const sänt1 = await kämpe.send(sträng)
-//    const sänt2 = await kämpe.send(sträng)
-//    const sänt3 = await kämpe.send(sträng)
-//    const reaktionsSamlare1 = sänt1.createReactionCollector({ time: 40_000 });
-//    const reaktionsSamlare2 = sänt2.createReactionCollector({ time: 40_000 });
-//    const reaktionsSamlare3 = sänt3.createReactionCollector({ time: 40_000 });
-//    reaktionsSamlare1.on('collect', (reaction, user) => {
-//      if(reaction.count == 2){
-//        console.log(1,EMOJI.indexOf(reaction._emoji.name))
-//        plan[1][EMOJI.indexOf(reaction._emoji.name)] = score
-//        reaktionsSamlare.stop();
-//      };
-//    });
-//    reaktionsSamlare.on('end', (reaction, user) => {
-//      //return fast async??
-//      jagKirrar(pick);
-//    });
-//    sänt.react(EMOJI[0]);
-//    sänt.react(EMOJI[1]);
-//    sänt.react(EMOJI[2]);
-//  });
-//  return jagLovarPåHederOchSamvete
-//};
-//
-//function harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan){
-//  let kolonnsummor = [0,0,0]
-//  let radsummor = [0,0,0]
-//  let diagonalsummor = [0,0]
-//  let storlek = plan.length
-//  for (let i = 0; i < storlek; i++){
-//      for (let j = 0; j < storlek; j++){
-//          kolonnsummor[j] += plan[i][j]
-//          radsummor[i] += plan[i][j]
-//          if(i==j){ diagonalsummor[0] += plan[i][j] }
-//          if(i==storlek-j-1){ diagonalsummor[1] += plan[i][j] }     
-//      }
-//  }
-//  kolonnsummor = Math.abs(kolonnsummor)
-//  radsummor = Math.abs(radsummor)
-//  diagonalsummor = Math.abs(diagonalsummor)
-//  return Math.max(kolonnsummor, radsummor, diagonalsummor) > 2
-//}
+async function treIRad(hurGårDet,utmanare,utmanad,igen = false) {
+  plan = [ [0,0,0],[0,0,0],[0,0,0] ]
+  rundaDelatPåTvå = 0 // Det blir ju max 9 rundor = 8 rundor + en som är tvingad = 4*2 rundor
+  över = false
+  console.log("Börjar ny tre-i-rad-match.")
+  while(!över){ //Loopa här tills någon vinner
+    plan = await skickaTreIRadUtmaning(plan,KRYSS,utmanare, igen)
+    över = harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)
+    hurGårDet.vinnare = utmanare
+    if(!över){
+      plan = await skickaTreIRadUtmaning(plan,CIRKEL,utmanad,igen)  
+      över = harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)
+      hurGårDet.vinnare = utmanad
+    }
+    rundaDelatPåTvå += 1
+    if(!över && rundaDelatPåTvå > 3){
+      över = true
+      // Gör det sista draget automatiskt och kolla ifall någon vann
+      for (let i = 0; i < 3; i++){
+        if(plan[i].includes(0)){
+          plan[i][plan[i].indexOf(0)] = 1 //1 börjar alltid så hen har sista draget
+        }
+      }
+      console.log(plan)
+      if(harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan)){
+        hurGårDet.vinnare = utmanare
+      } else {
+        utmanad.send("Rats! It's a tie. Here's the final board:")
+        utmanare.send("Rats! It's a tie. Here's the final board:")
+        sändPlan(plan,utmanad)
+        sändPlan(plan,utmanare)
+        hurGårDet.vinnare = undefined
+      }
+    }
+  }
+  hurGårDet.jättebra = true
+  hurGårDet.meddelande = "Rätt bra faktiskt!"
+  if(hurGårDet.vinnare != undefined){
+    if(hurGårDet.vinnare == utmanare){
+      utmanare.send("Number one victory real B)")
+      utmanare.send("Here's the final board:")
+      utmanad.send("You lost... but they probably cheated anyways.")
+      utmanad.send("Here's the final board:")
+      sändPlan(plan,utmanad)
+      sändPlan(plan,utmanare)
+    } else {
+      utmanad.send("Number one victory real B)")
+      utmanad.send("Here's the final board:")
+      utmanare.send("You lost... but they probably cheated anyways.")
+      utmanare.send("Here's the final board:")
+      sändPlan(plan,utmanad)
+      sändPlan(plan,utmanare)
+    }
+    hurGårDet.vinnare.send("You won! Congratulations :)")
+  }
+  return hurGårDet 
+}
+
+async function sändPlan(plan,kämpe){
+  poängEmoji = {
+    "-1": CIRKEL,
+    "0":  KVADRAT,
+    "1":  KRYSS,
+  }
+  
+  sträng = ["","",""]
+  for (let i=0; i < 3; i++){
+    sträng[i] = ""
+    for (let j=0; j < 3; j++){
+      sträng[i] += poängEmoji[plan[i][j]][0]
+    }
+  }
+  await kämpe.send(sträng[0])
+  await kämpe.send(sträng[1])
+  await kämpe.send(sträng[2])
+};
+
+
+async function skickaTreIRadUtmaning( plan, EMOJI, kämpe, score, igen = false){
+  const jagLovarPåHederOchSamvete = new Promise(async (jagKirrar, jagBajsar) => {
+    
+    poängEmoji = {
+      "-1": CIRKEL,
+      "0":  KVADRAT,
+      "1":  KRYSS,
+    }
+
+    if(EMOJI == KRYSS){
+      score = 1
+    } else {
+      score = -1
+    }
+
+    sträng = ["","",""]
+    for (let i=0; i < 3; i++){
+      sträng[i] = ""
+      for (let j=0; j < 3; j++){
+        sträng[i] += poängEmoji[plan[i][j]][0]
+      }
+    }
+    
+    console.log(sträng)
+    
+    const sänt0 = await kämpe.send(sträng[0])
+    const sänt1 = await kämpe.send(sträng[1])
+    const sänt2 = await kämpe.send(sträng[2])
+    const reaktionsSamlare0 = sänt0.createReactionCollector({ time: 40_000 });
+    const reaktionsSamlare1 = sänt1.createReactionCollector({ time: 40_000 });
+    const reaktionsSamlare2 = sänt2.createReactionCollector({ time: 40_000 });
+    reaktionsSamlare0.on('collect', (reaction, user) => {
+      if(reaction.count == 2){
+        index = EMOJI.indexOf(reaction._emoji.name)
+        if (plan[0][index] == 0){
+          plan[0][index] = score
+          reaktionsSamlare0.stop();
+          reaktionsSamlare1.stop();
+          reaktionsSamlare2.stop();
+          jagKirrar(plan);
+        };
+      };
+    });
+    reaktionsSamlare1.on('collect', (reaction, user) => {
+      if(reaction.count == 2){
+        index = EMOJI.indexOf(reaction._emoji.name)
+        if (plan[1][index] == 0){
+          plan[1][index] = score
+          reaktionsSamlare0.stop();
+          reaktionsSamlare1.stop();
+          reaktionsSamlare2.stop();
+          jagKirrar(plan);
+        };
+      };
+    });
+    reaktionsSamlare2.on('collect', (reaction, user) => {
+      if(reaction.count == 2){
+        index = EMOJI.indexOf(reaction._emoji.name)
+        if (plan[2][index] == 0){
+          plan[2][index] = score
+          reaktionsSamlare0.stop();
+          reaktionsSamlare1.stop();
+          reaktionsSamlare2.stop();
+          jagKirrar(plan);
+        };
+      };
+    });
+    for (let i = 0; i<3; i++){
+      sänt0.react(EMOJI[i]);
+      sänt1.react(EMOJI[i]);
+      sänt2.react(EMOJI[i]);
+    }
+  });
+  return jagLovarPåHederOchSamvete
+};
+
+function harNågonVunnitPåDenHärPlanenNuEllerSkaViKanskeKöraEnRundaTill(plan){
+  let kolonnsummor = [0,0,0]
+  let radsummor = [0,0,0]
+  let diagonalsummor = [0,0]
+  for (let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++){
+          kolonnsummor[j] += plan[i][j]
+          radsummor[i] += plan[i][j]
+          if(i==j){ diagonalsummor[0] += plan[i][j] }
+          if(i==2-j){ diagonalsummor[1] += plan[i][j] }     
+      }
+  }
+  console.log("HÄR ÄR SUMMORNA")
+  console.log(kolonnsummor,radsummor,diagonalsummor)
+  // Max of abs of array
+  kolonnsumma = Math.max.apply(null, kolonnsummor.map(Math.abs));
+  radsumma = Math.max.apply(null, radsummor.map(Math.abs));
+  diagonalsumma = Math.max.apply(null, diagonalsummor.map(Math.abs));
+  return Math.max(kolonnsumma, radsumma, diagonalsumma) > 2
+}
 	
 // STEN SAX PÅSE!!1
     
