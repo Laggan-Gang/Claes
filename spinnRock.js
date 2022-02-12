c7e26583d49e730b004e60d778de05e4dddf9685
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-02-12 18:34:33 +0100

    Let's hope for the best!

--- spinnRock.js
+++ spinnRock.js
@@ -287,7 +287,7 @@ module.exports = {
         }
 
         const end = performance.now();
-        const elapsedTime = timeToPick || Math.trunc(end - start);
+        const elapsedTime = Math.trunc(end - start);
 
         let modRader = modMeddelande.split('\n');
 
